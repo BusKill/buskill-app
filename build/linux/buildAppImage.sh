1ec55f06328051c7dd2bce3e4dbcc0c529bad0f6
Michael Altfield <michael@michaelaltfield.net>
2020-05-30 16:39:38 +0545

    added some polish to the linux appimage build script, including better comments, referencing the requirements.txt file, and explicitly setting the path to python3.7 (before we had just used `python3` which could be an issue if buster-slim's default becomes `python3.8` but kivy is still pinned to `python3.7`

--- build/linux/buildAppImage.sh
+++ build/linux/buildAppImage.sh
@@ -14,13 +14,34 @@
 # Version: 0.2
 ################################################################################
 
-# install depends
-apt-get update; apt-get -y install python3-pip wget rsync fuse
+############
+# SETTINGS #
+############
 
-pip3 install --upgrade --user pip setuptools virtualenv
-python3 -m virtualenv /tmp/kivy_venv
-source /tmp/kivy_venv/bin/activate; python3 -m pip install -r requirements.txt
+PYTHON_PATH='/usr/bin/python3.7'
 
+###################
+# INSTALL DEPENDS #
+###################
+
+# install os-level depends
+apt-get update; apt-get -y install python3.7-pip wget rsync fuse
+
+# setup a virtualenv to isolate our app's python depends
+${PYTHON_PATH} -m pip install --upgrade --user pip setuptools virtualenv
+${PYTHON_PATH} -m virtualenv /tmp/kivy_venv
+
+# install kivy and all other python dependencies with pip into our virtual env
+# we'll later add these to our AppDir for building the AppImage
+source /tmp/kivy_venv/bin/activate; ${PYTHON_PATH} -m pip install -r requirements.txt
+
+##################
+# PREPARE APPDIR #
+##################
+
+# download the latest python-appimage release, which is an AppImage containing
+# the core python3.7 runtime. We use this as a base for building our own python
+# AppImage. We only have to add our code and depends to it.
 wget -O /tmp/python3.7.AppImage https://github.com/niess/python-appimage/releases/download/python3.7/python3.7.7-cp37-cp37m-manylinux2014_x86_64.AppImage
 chmod +x /tmp/python3.7.AppImage
 /tmp/python3.7.AppImage --appimage-extract
@@ -94,6 +115,10 @@ EOF
 # make it executable
 chmod +x /tmp/kivy_appdir/AppRun
 
+##################
+# BUILD APPIMAGE #
+##################
+
 # create the AppImage from kivy AppDir
 wget -O /tmp/appimagetool.AppImage https://github.com/AppImage/AppImageKit/releases/download/12/appimagetool-x86_64.AppImage
 chmod +x /tmp/appimagetool.AppImage
@@ -102,3 +127,6 @@ chmod +x /tmp/appimagetool.AppImage
 # note tha gitlab will only accept artifacts that are in the build dir (cwd)
 mkdir dist
 /tmp/appimagetool.AppImage /tmp/kivy_appdir dist/helloWorld.AppImage
+
+# exit cleanly
+exit 0
