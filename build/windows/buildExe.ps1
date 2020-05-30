c39a3fcea53af065fa994dd4f05dc564aef0e97e
Michael Altfield <michael@michaelaltfield.net>
2020-05-31 03:48:17 +0545

    I'm having this weird issue where PowerShell just fucking isn't executing top-down. Shit's just popping up wherever, making the ouput impossible to read and the logic impossible to flow. I'm trying this hack to force it to be deterministic
     * https://stackoverflow.com/questions/1741490/how-to-tell-powershell-to-wait-for-each-command-to-end-before-starting-the-next

--- build/windows/buildExe.ps1
+++ build/windows/buildExe.ps1
@@ -1,38 +1,38 @@
 Set-PSDebug -Trace 1
 
 Write-Output "listing contents of C drive root"
-Get-ChildItem -Path C:\ -Force
+Get-ChildItem -Path C:\ -Force | Out-String
 
 Write-Output "listing contents of cwd"
-Get-ChildItem -Force
+Get-ChildItem -Force | Out-String
 
 Write-Output 'INFO: Beginning execution'
 
 Write-Output 'INFO: First check if GCP already has python?'
-python --version
-python.exe --version
+python --version | Out-String
+python.exe --version | Out-String
 
 # it looks like we can't use the smaller embeddable zip file as it lacks pip
 #curl -OutFile python3.7.zip https://www.python.org/ftp/python/3.7.7/python-3.7.7-embed-amd64.zip
 #Expand-Archive .\python3.7.zip
 
 Write-Output 'INFO: Downloading python3.7'
-curl -OutFile python3.7.exe https://www.python.org/ftp/python/3.7.7/python-3.7.7-amd64.exe
+curl -OutFile python3.7.exe https://www.python.org/ftp/python/3.7.7/python-3.7.7-amd64.exe | Out-String
 
 Write-Output 'INFO: Installing python'
-New-Item -Path C:\tmp -Type Directory
-New-Item -Path C:\tmp\python -Type Directory
-.\python3.7.exe /passive TargetDir=C:\tmp\python IncludePip=1
+New-Item -Path C:\tmp -Type Directory | Out-String
+New-Item -Path C:\tmp\python -Type Directory | Out-String
+.\python3.7.exe /passive TargetDir=C:\tmp\python IncludePip=1 | Out-String
 
-Write-Output 'INFO: Installing pip, setuptools, and virtualenv'
-C:\tmp\python\python.exe -m pip install --upgrade --user pip wheel setuptools virtualenv
+Write-Output 'INFO: Installing pip, setuptools, and virtualenv' | Out-String
+C:\tmp\python\python.exe -m pip install --upgrade --user pip wheel setuptools virtualenv | Out-String
 
 Write-Output 'INFO: Installing kivy'
-New-Item -Path C:\tmp\kivy_venv -Type Directory
-C:\tmp\python\python.exe -m virtualenv C:\tmp\kivy_venv
-C:\tmp\kivy_venv\Scripts\activate.ps1
-C:\tmp\kivy_venv\Scripts\python.exe -m pip install docutils pygments pypiwin32 kivy_deps.sdl2 kivy_deps.glew kivy_deps.angle
-C:\tmp\kivy_venv\Scripts\python.exe -m pip install kivy
+New-Item -Path C:\tmp\kivy_venv -Type Directory | Out-String
+C:\tmp\python\python.exe -m virtualenv C:\tmp\kivy_venv | Out-String
+C:\tmp\kivy_venv\Scripts\activate.ps1 | Out-String
+C:\tmp\kivy_venv\Scripts\python.exe -m pip install docutils pygments pypiwin32 kivy_deps.sdl2 kivy_deps.glew kivy_deps.angle | Out-String
+C:\tmp\kivy_venv\Scripts\python.exe -m pip install kivy | Out-String
 
 Write-Output 'INFO: Prepare our exe'
 #C:\tmp\kivy_venv\Scripts\python.exe -m pip install pyinstaller
@@ -42,11 +42,11 @@ Write-Output 'INFO: Prepare our exe'
 
 # First let's confirm that we can get the example in the docs to work
 #  * https://kivy.org/doc/stable/guide/packaging-windows.html
-C:\tmp\kivy_venv\Scripts\python.exe -m pip install kivy_examples
-C:\tmp\kivy_venv\Scripts\python.exe -m PyInstaller --name touchtracer C:\tmp\kivy_venv\share\kivy-examples\demo\touchtracer\main.py
+C:\tmp\kivy_venv\Scripts\python.exe -m pip install kivy_examples | Out-String
+C:\tmp\kivy_venv\Scripts\python.exe -m PyInstaller --name touchtracer C:\tmp\kivy_venv\share\kivy-examples\demo\touchtracer\main.py | Out-String
 
 # replace spec file
-mv touchtracer.spec touchtracer.orig
+mv touchtracer.spec touchtracer.orig | Out-String
 echo "# -*- mode: python ; coding: utf-8 -*-
 from kivy_deps import glew, sdl2
 
@@ -89,4 +89,4 @@ coll = COLLECT(exe, Tree('C:\\tmp\\kivy_venv\\share\\kivy-examples\\demo\\toucht
 " | tee touchtracer.spec
 
 # build it from the spec file
-C:\tmp\kivy_venv\Scripts\python.exe -m PyInstaller --name touchtracer touchtracer.spec
+C:\tmp\kivy_venv\Scripts\python.exe -m PyInstaller --name touchtracer touchtracer.spec | Out-String
