f539a9449496025ad8c0ed13517a805575804c8c
Michael Altfield <michael@michaelaltfield.net>
2020-05-31 23:35:15 +0545

    ok, the build from the last commit in linux worked again. I guess windows requires the env variable KIVY_GL_BACKEND to be angle but linux breaks with it set. The solution should probably be to put that in some conditional statement. First let me try this.

--- src/main.py
+++ src/main.py
@@ -3,6 +3,9 @@
 #import os
 #os.environ['KIVY_GL_BACKEND'] = 'angle_sdl2'
 
+from kivy import Config
+Config.set('graphics', 'multisamples', '0')
+
 import kivy
 #kivy.require('1.0.6') # replace with your current kivy version !
 
