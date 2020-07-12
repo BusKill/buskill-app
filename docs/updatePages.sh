cec4fa8ebad25aaa469a3e936738f2d174f569f7
Michael Altfield <michael@michaelaltfield.net>
2020-07-13 05:39:28 +0545

    Moving this repo's documentation into a repo-specific subdir
    Since the last commit, I was successfully able to get docs.buskill.in to redirect to this repo's GitHub Pages, but
    
     1. There was no way to have this repo in a specific dir in that config. In the future, I might want to use docs.buskill.in for hosting more docs than just for this app, so I'm moving it into a repo-docs-specific dir so permalinks will always work. But since the only docs we have is this one repo's dir, I'm just creating a index.html file that does a meta redirect to the dir.
    
     2. The way that GitHub Pages sets up the custom domain depends on me changing my DNS server to CNAME and also there needs to be a '/CNAME' file in the "docroot". If I understood the docs correctly, if this file disappears then anyone on github might be able to steal our custom domain. So I added a step to our docs build script that creates this file as needed every time.

--- docs/updatePages.sh
+++ docs/updatePages.sh
@@ -55,7 +55,7 @@ git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 
 docroot=`mktemp -d`
-rsync -av "docs/_build/html/" "${docroot}/"
+rsync -av "docs/_build/html/" "${docroot}/buskill-app/"
 
 pushd "${docroot}"
 
@@ -64,11 +64,28 @@ git init
 git remote add deploy "https://token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 git checkout -b gh-pages
 
+# Add CNAME - this is required for GitHub to know what our custom domain is
+echo "docs.buskill.in" > CNAME
+
+# add redirect (for now) since I want repo-specific docs dirs, but we only have one so far
+cat >> index.html <<EOF
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>BusKill Docs</title>
+      <meta http-equiv = "refresh" content="0; url='/buskill-app/'" />
+   </head>
+   <body>
+      <p>Please wait while you're redirected to our <a href="/buskill-app/">buskill-app documentation page</a>.</p>
+   </body>
+</html>
+EOF
+
 # Add README
 cat >> README.md <<EOF
 # GitHub Pages Cache
 
-Nothing to see here. The contents of this dir are merely a cache that's not intended to be viewed on github.com.
+Nothing to see here. The contents of this branch are essentially a cache that's not intended to be viewed on github.com.
 
 You can view the actual documentation as it's intended to be viewed at [https://docs.buskill.in/](https://docs.buskill.in/)
 
