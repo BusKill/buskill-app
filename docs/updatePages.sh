46b302f8fb365971e3b0449062de166a0c14c75f
Michael Altfield <michael@michaelaltfield.net>
2020-07-13 04:59:06 +0545

    The last commit worked! Now when we're pushing to github.com, our documentation is automatically generated with sphinx, pushed to the 'gh-pages' branch, and our webpage is updated.
    This commit adds a README file to that branch just in-case anyone ends up there looking for the documentation.
    
    The next task (before actually writing documentation) is to get docs.buskill.in custom domain setup for GitHub Pages.

--- docs/updatePages.sh
+++ docs/updatePages.sh
@@ -64,6 +64,17 @@ git init
 git remote add deploy "https://token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 git checkout -b gh-pages
 
+# Add README
+cat >> README.md <<EOF
+# GitHub Pages Cache
+
+Nothing to see here. The contents of this dir are merely a cache that's not intended to be viewed on github.com.
+
+You can view the actual documentation as it's intended to be viewed at [https://docs.buskill.in/](https://docs.buskill.in/)
+
+If you're looking to update our documentation, check the relevant development branch's ['docs' dir](https://github.com/BusKill/buskill-app/tree/master/docs).
+EOF
+
 # copy the resulting html pages built from sphinx above to our new git repo
 git add .
 
