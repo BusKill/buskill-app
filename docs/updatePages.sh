adc2a13d859ac0a7a1387808258efb558d14f4cc
Michael Altfield <michael@michaelaltfield.net>
2020-07-13 03:09:03 +0545

    attempting to actually update the gh-pages branch. Hopefully this won't break master (again) x_x

--- docs/updatePages.sh
+++ docs/updatePages.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -x
 ################################################################################
-# File:    signRelease.sh
-# Purpose: Script that signs builds our documentation using sphinx and updates
-#          GitHub Pages. This script is executed by:
+# File:    updatePages.sh
+# Purpose: Script that builds our documentation using sphinx and updates GitHub
+#          Pages. This script is executed by:
 #            .github/workflows/docs_pages_workflow.yml
 #
 # Authors: Michael Altfield <michael@buskill.in>
@@ -16,7 +16,7 @@ set -x
 #                                  SETTINGS                                    #
 ################################################################################
 
-# n/a
+export SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct)
 
 ################################################################################
 #                                 MAIN BODY                                    #
@@ -27,17 +27,39 @@ set -x
 ###################
 
 apt-get update
-apt-get -y install git python3-sphinx
+apt-get -y install git python3-sphinx rsync
 
 ##############
 # BUILD DOCS #
 ##############
 
-sandbox=`mktemp -d`
-pushd "${tmpDir}"
+# build our documentation with sphinx (see docs/conf.py)
+# * https://www.sphinx-doc.org/en/master/usage/quickstart.html#running-the-build
+make -C docs clean
+make -C docs html
 
-# TODO clone repo using env vars
-env
+#######################
+# Update GitHub Pages #
+#######################
+
+docroot=`mktemp -d`
+pushd "${docroot}"
+
+# don't bother maintaining history; just generate fresh
+git init
+git remote add origin "https://token@${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+git checkout -b gh-pages
+
+# copy the resulting html pages built from sphinx above to our new git repo
+rsync -av "docs/_build/html/" "${pagesSandbox}/"
+git add .
+
+# commit all the new files
+msg="Updating Docs for commit ${GITHUB_SHA} made on `date -d"@${SOUCE_DATE_EPOCH}" --iso-8601=seconds` from ${GITHUB_REF}"
+git commit -am "${msg}"
+
+# overwrite the contents of the gh-pages branch on our github.com repo
+git push origin gh-pages --force
 
 ##################
 # CLEANUP & EXIT #
