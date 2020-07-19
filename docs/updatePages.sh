a98e76fb024466f06fa0393f66c0b3e0896f01e2
Michael Altfield <michael@michaelaltfield.net>
2020-07-19 18:52:40 +0545

    adding two new depends for iterating over languages and branches/versions:
     1. python3-stemmer
     2. python3-git

--- docs/updatePages.sh
+++ docs/updatePages.sh
@@ -27,7 +27,7 @@ set -x
 ###################
 
 apt-get update
-apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme
+apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme python3-stemmer python3-git
 
 #####################
 # DECLARE VARIABLES #
