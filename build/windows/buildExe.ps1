a7e14d62fae25a52411f9de643a711f3718a8c76
Michael Altfield <michael@michaelaltfield.net>
2020-08-20 01:22:47 +0545

    attempting to fix Tk issue
    Well the last build now just exits prematurely with no log or anything with this error message spat to the terminal:
    
    	C:\Users\user\Downloads\buskill-windows-x86_64.215632304\buskill-x86_64>buskill -U
      	Traceback (most recent call last):
      	File "PyInstaller\hooks\rthooks\pyi_rth__tkinter.py", line 22, in <module>
    	FileNotFoundError: Tk data directory "C:\Users\user\Downloads\buskill-windows-x86_64.215632304\buskill-x86_64\tk" not found.
    	[4496] Failed to execute script pyi_rth__tkinter
    
    	C:\Users\user\Downloads\buskill-windows-x86_64.215632304\buskill-x86_64>
    
    But then on double-tap that error went away, but a new one cropped-up:
    
    	C:\Users\user\Downloads\buskill-windows-x86_64.215632304\buskill-x86_64>buskill -U
    buskill version {'GITHUB_REF': 'refs/heads/dev', 'GITHUB_SHA': '525d95dd65f56922bfc5957242feb73617d4602f', 'SOURCE_DATE_EPOCH': '1597861627'}
    	Unable to write to 'C:\Users\user\Downloads\buskill-windows-x86_64.215632304\buskill-x86_64\base_library.zip'; skipping.
            	[Errno 2] No such file or directory: 'C:\\Users\\user\\Downloads\\buskill-windows-x86_64.215632304\\buskill-x86_64\\base_library.zip\\tmp31ur6n0_'
    
    	DEBUG: using DATA_DIR:|C:\Users\user\Downloads\buskill-windows-x86_64.215632304\buskill-x86_64\.buskill|
    	Traceback (most recent call last):
      	File "main.py", line 103, in <module>
        	ret = BusKillCLI()
      	File "buskill_cli.py", line 84, in BusKillCLI
        	buskill.upgrade()
      	File "buskill.py", line 483, in upgrade
        	gpg = gnupg.GPG( gnupghome=GNUPGHOME )
      	File "gnupg.py", line 863, in __init__
    	ValueError: Error invoking gpg: 3221225781:
    	[5744] Failed to execute script main
    
    	C:\Users\user\Downloads\buskill-windows-x86_64.215632304\buskill-x86_64>
    
    I'll attempt to fix the first issue with --onefile per this, but it may not be relevant (and I want to switch to --onefile anyway for cleaner upgrades)
    
     * https://github.com/pyinstaller/pyinstaller/issues/1957
    
    Before the latter error, I got the same DLL error as before for the following new files, which I'll fix in the next commit:
    
     * msys-2.0.dll
     * msys-readline8.dll
     * msys-z.dll
     * msys-sqlite3-0.dll

--- build/windows/buildExe.ps1
+++ build/windows/buildExe.ps1
@@ -196,7 +196,7 @@ coll = COLLECT(exe, Tree('..\\src\\'),
 $env:KIVY_GL_BACKEND="angle_sdl2"
 
 # build it from the spec file
-C:\tmp\kivy_venv\Scripts\python.exe -m PyInstaller --noconfirm .\buskill.spec | Out-String
+C:\tmp\kivy_venv\Scripts\python.exe -m PyInstaller --noconfirm --onefile .\buskill.spec | Out-String
 
 # attempt to execute it?
 #.\dist\buskill\buskill.exe | Out-String
