b70596aaf8048dd40bf67ad0f4cca2f25b93eff5
Michael Altfield <michael@michaelaltfield.net>
2020-09-07 00:39:13 +0545

    removing garden from the cleanup in the build script
    user@disp6156:~/buskill-lin-1599417311-x86_64$ ./buskill-1599417311.AppImage
    buskill version {'VERSION': '1599417311', 'GITHUB_REF': 'refs/heads/dev', 'GITHUB_SHA': 'aea6f23b006bf08727cac2b0689f8922bdc29533', 'SOURCE_DATE_EPOCH': '1599417311'}
    No command-line arguments detected. Launching GUI
    Hint: execute `buskill --help` for command-line usage
    [INFO   ] [Logger      ] Record log in /home/user/.kivy/logs/kivy_20-09-07_2.txt
    [INFO   ] [Kivy        ] v1.11.1
    [INFO   ] [Kivy        ] Installed at "/tmp/.mount_buskil8kfsMT/opt/python3.7/lib/python3.7/site-packages/kivy/__init__.py"
    [INFO   ] [Python      ] v3.7.8 (default, Jul  4 2020, 10:00:57)
    [GCC 9.3.1 20200408 (Red Hat 9.3.1-2)]
    [INFO   ] [Python      ] Interpreter at "/home/user/buskill-lin-1599417311-x86_64/buskill-1599417311.AppImage"
    [INFO   ] [Factory     ] 184 symbols loaded
    [INFO   ] [Image       ] Providers: img_tex, img_dds, img_sdl2, img_gif (img_pil, img_ffpyplayer ignored)
    [INFO   ] [Window      ] Provider: sdl2(['window_egl_rpi'] ignored)
    [INFO   ] [GL          ] Using the "OpenGL" graphics system
    [INFO   ] [GL          ] Backend used <sdl2>
    [INFO   ] [GL          ] OpenGL version <b'3.1 Mesa 18.3.6'>
    [INFO   ] [GL          ] OpenGL vendor <b'VMware, Inc.'>
    [INFO   ] [GL          ] OpenGL renderer <b'llvmpipe (LLVM 7.0, 256 bits)'>
    [INFO   ] [GL          ] OpenGL parsed version: 3, 1
    [INFO   ] [GL          ] Shading version <b'1.40'>
    [INFO   ] [GL          ] Texture max size <8192>
    [INFO   ] [GL          ] Texture max units <32>
    [INFO   ] [Window      ] auto add sdl2 input provider
    [INFO   ] [Window      ] virtual keyboard not allowed, single mode, not docked
    [INFO   ] [Text        ] Provider: sdl2
     Traceback (most recent call last):
       File "/tmp/.mount_buskil8kfsMT/opt/src/main.py", line 96, in <module>
         from buskill_gui import BusKillApp
       File "/tmp/.mount_buskil8kfsMT/opt/src/buskill_gui.py", line 48, in <module>
         from garden.navigationdrawer import NavigationDrawer
     ModuleNotFoundError: No module named 'garden'
    [INFO/MainProcess] process shutting down
    [DEBUG/MainProcess] running all "atexit" finalizers with priority >= 0
    [DEBUG/MainProcess] running the remaining "atexit" finalizers
    user@disp6156:~/buskill-lin-1599417311-x86_6user@disp6156:~/buskill-lin-1599417311-x86_6user@disp6156:~/buskill-lin-1599417311-x8user@disp6156:~/buskill-lin-1599417311-user@disp6156:~/buskill-lin-1599417311-x86_6user@disp6user@disp6156:~/user@disuseruser@disp6156:~/buskill-lin-1599417311-x86_64$

--- build/linux/buildAppImage.sh
+++ build/linux/buildAppImage.sh
@@ -222,7 +222,7 @@ echo "INFO: Beginning AppDir thinning"
 # remove some unnecessary items from the AppDir to reduce the AppImage size
 # and make the AppImage reproducible
 
-unnecessary="__pycache__ pip pygments docutils setuptools chardet urllib3 elftools pkg_resources garden kivy-examples requests direct_url.json RECORD"
+unnecessary="__pycache__ pip pygments docutils setuptools chardet urllib3 elftools pkg_resources kivy-examples requests direct_url.json RECORD"
 for item in $(echo "${unnecessary}"); do
 
 	paths=`find /tmp/kivy_appdir -iname "*${item}*"`
