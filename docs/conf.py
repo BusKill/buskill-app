1c01774c684d2198812b9c293026734642537fa2
Michael Altfield <michael@michaelaltfield.net>
2020-09-26 18:34:44 +0545

    adding favico to sphinx config for docs.buskill.in

--- docs/conf.py
+++ docs/conf.py
@@ -89,6 +89,7 @@ pygments_style = None
 #
 html_theme = 'sphinx_rtd_theme'
 html_logo = '_static/buskill_202007_200px.png'
+html_favicon = '_static/favico_150b.202002.png'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
