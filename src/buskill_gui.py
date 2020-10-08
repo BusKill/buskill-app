357056e35518a9fd17650d869b0aefea321b4b8c
Michael Altfield <michael@michaelaltfield.net>
2020-10-08 16:53:47 +0545

    to use the right instance's name

--- src/buskill_gui.py
+++ src/buskill_gui.py
@@ -286,8 +286,8 @@ class MainWindow(BoxLayout):
 
 			# cleanup env; remove references to now-old version
 			oldVersionPaths = [
-			 os.path.split(sys.argv[0])[0],
-			 os.path.split(self.APP_DIR)[1]
+			 os.path.split( sys.argv[0] )[0],
+			 os.path.split( bk.APP_DIR )[1]
 			]
 			# TODO: remove me
 			msg = 'DEBUG: removing oldVersionPaths from PATH (' +str(oldVersionPaths)+ ')'
