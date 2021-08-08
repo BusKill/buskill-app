6a4d83d13c8da91b24274c5c0352fc4e806b1be3
Michael Altfield <michael@michaelaltfield.net>
2021-08-08 23:58:05 +0200

    same for pip
    The last commit appears to be setting the python path correctly after
    install. This commit attempts to do the same with pip

--- build/mac/buildDmg.sh
+++ build/mac/buildDmg.sh
@@ -140,6 +140,12 @@ cat ${PIP_PATH}
 # get python essential dependencies
 ${PIP_PATH} install --ignore-installed --upgrade --cache-dir build/deps/ --no-index --find-links file://`pwd`/build/deps/ build/deps/pip-20.1.1-py2.py3-none-any.whl
 PIP_PATH="`find /usr/local/Cellar/python@3* -type f -wholename *bin/pip3* | sort -n | uniq | head -n1`"
+PIP_PATH="`find /usr/local/Cellar/python/ -type f -wholename *bin/pip3* | sort -n | uniq | head -n1`"
+
+# get more info post-pip install
+ls -lah /usr/local/Cellar/python/
+find /usr/local/Cellar/python/ -type f -wholename *bin/python3*
+find /usr/local/Cellar/python/* -type f -wholename *bin/pip3*
 
 ${PIP_PATH} install --ignore-installed --upgrade --cache-dir build/deps/ --no-index --find-links file://`pwd`/build/deps/ build/deps/setuptools-49.1.0-py3-none-any.whl
 ${PIP_PATH} install --ignore-installed --upgrade --cache-dir build/deps/ --no-index --find-links file://`pwd`/build/deps/ build/deps/wheel-0.34.2-py2.py3-none-any.whl
