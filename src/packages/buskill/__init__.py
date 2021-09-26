e3f047dcb6ea50ca05b4e8b1e925a6939b4f920f
Michael Altfield <github_maltfield@michaelaltfield.net>
2021-09-26 18:16:42 +0200

    Update __init__.py

--- src/packages/buskill/__init__.py
+++ src/packages/buskill/__init__.py
@@ -3,7 +3,8 @@
 ::
 
   File:    buskill.py
-  Authors: Michael Altfield <michael@buskill.in>, Steven Johnson <steven.j2019@protonmail.com>
+  Authors: Michael Altfield <michael@buskill.in>
+  Co-Auth: Steven Johnson <steven.j2019@protonmail.com>
   Created: 2020-06-23
   Updated: 2020-11-30
   Version: 0.2
