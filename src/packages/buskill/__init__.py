45846003aca2d1e1dbc90eea9752b88a7c615836
Michael Altfield <michael@michaelaltfield.net>
2022-10-07 12:59:05 -0500

    fix bug where we can't caputre the output of pmset execution because it wasn't assigned to result!

--- src/packages/buskill/__init__.py
+++ src/packages/buskill/__init__.py
@@ -756,7 +756,10 @@ class BusKill:
 			# that didn't work; log it and try fallback
 			msg = "INFO: Attempting to execute `pmset displaysleepnow`"
 			print( msg ); logger.debug( msg )
-			subprocess.run( ['pmset', 'displaysleepnow'], capture_output=True, text=True )
+			result = subprocess.run(
+			 ['pmset', 'displaysleepnow'],
+			 capture_output=True, text=True
+			)
 
 			msg = "DEBUG: subprocess returncode|" +str(result.returncode)+ "|"
 			print( msg ); logger.debug( msg )
