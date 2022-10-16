7145b6e31a70def75ded52c7ec39e6954fc17f48
Michael Altfield <michael@michaelaltfield.net>
2022-10-15 22:26:53 -0500

    attempting to fix permission errors on chown
     * https://github.com/BusKill/buskill-app/actions/runs/3257959253/jobs/5349638757
    
      2022-10-16T02:45:22.6646310Z + root_child_path=buskill-cli-shutdown-trigger.app/Contents/MacOS/packages/buskill/root_child_mac.py
      2022-10-16T02:45:22.6647010Z + chown 0:0 buskill-cli-shutdown-trigger.app/Contents/MacOS/packages/buskill/root_child_mac.py
      2022-10-16T02:45:22.7611510Z chown: buskill-cli-shutdown-trigger.app/Contents/MacOS/packages/buskill/root_child_mac.py: Operation not permitted
      2022-10-16T02:45:22.7614470Z + chmod 0400 buskill-cli-shutdown-trigger.app/Contents/MacOS/packages/buskill/root_child_mac.py

--- build/mac/buildDmg.sh
+++ build/mac/buildDmg.sh
@@ -354,8 +354,8 @@ ls -lah
 # * https://github.com/BusKill/buskill-app/issues/14#issuecomment-1272449172
 
 root_child_path="${APP_DIR_NAME}/Contents/MacOS/packages/buskill/root_child_mac.py"
-chown 0:0 "${root_child_path}"
-chmod 0400 "${root_child_path}"
+sudo chown 0:0 "${root_child_path}"
+sudo chmod 0400 "${root_child_path}"
 
 ########################
 # ADD ADDITIONAL FILES #
