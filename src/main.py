a7bc66368525e69f220f4fcdcf5074f7b17c4f12
Raihan93-coder <raihanshamnad93@gmail.com>
2026-05-25 22:32:34 +0530

    Fix user-specific log file initialization

--- src/main.py
+++ src/main.py
@@ -54,7 +54,11 @@ if __name__ == '__main__':
 
 	# TODO: disable logging by default; enable it with an argument
 	# TODO: be able to override the path to the log file with an env var or argument value; make these just the defaults
-	log_file_path = os.path.join( tempfile.gettempdir(), 'buskill.log' )
+
+    # Removing the universal log file logic that generates the file in /tmp dir
+    # Changing the location of the log file to the user specific cache thus preventing the file permission error 
+	buskill = packages.buskill.BusKill()
+	log_file_path = os.path.join( buskill.CACHE_DIR, 'buskill.log' )
 
 	logging.basicConfig(
 	 filename = log_file_path,
