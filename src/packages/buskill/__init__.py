b34683bde77346cf4f62ba2b6f392d4a0a0c24c2
Ahmed Fatih Akpolat <85339585+afatihakpolat@users.noreply.github.com>
2026-09-07 17:58:49 -0400

    fix: tolerate log handlers without baseFilename (pytest compat, #131)

--- src/packages/buskill/__init__.py
+++ src/packages/buskill/__init__.py
@@ -227,7 +227,7 @@ class BusKill:
 
 		# BusKill class was always initialised after finding the log file path Therefore in the new setup we are initialising it before the log file path is found
 		# This will prevent it from out of index error 
-		self.LOG_FILE_PATH = logger.root.handlers[0].baseFilename if logger.root.handlers else None
+		self.LOG_FILE_PATH = getattr( logger.root.handlers[0], 'baseFilename', None ) if logger.root.handlers else None
 		# Default value as False, because if no one updates the config.ini file then reverting back to old file path
 		self.PERSISTENT_LOG = False
 		
